bd74323f5c1ac61493275cfa27b651c1e3b3896b
Muzammil <emailtomuzammil@gmail.com>
2017-03-07 16:52:06 +0100

    Login Interfacce

---
+++ OnBoarding/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace OnBoarding
+{
+	public partial class LoginPage : PageBase
+	{
+		private readonly Lazy<LoginViewModel> _typedViewModel = new Lazy<LoginViewModel>();
+
+		public override ViewModelBase ViewModel => _typedViewModel.Value;
+
+		public LoginPage()
+		{
+			InitializeComponent();
+
+			BindingContext = ViewModel;
+		}
+	}
+}
